b4a77d375831d00063e1c722cf61df95ce661d81
Tiheros <ipz23-i.dyshliuk@nubip.edu.ua>
2024-07-09 23:24:05 +0300

    lab1

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
     double t1, t2, t3;
-    printf("Enter three values (t1, t2, t3): ");
+    printf("Enter three values: ");
     scanf("%lf %lf %lf", &t1, &t2, &t3);
-    
-    double total_time = 1 / (1/t1 + 1/t2 + 1/t3);
-    printf("Time required to eat the pie: %.2f hours\n", total_time);
+    double total = (1.0 / t1) + (1.0 / t2) + (1.0 / t3);
+    double time_total = 1.0 / total;
+    printf("Time: %.2f hours\n", time_total);
     
     return 0;
 }
